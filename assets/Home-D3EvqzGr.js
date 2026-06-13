172a7360ba04aa8a21b52d74854d1038ca9c5d87
therapon1997-DERMLUX <therapon1997-DERMLUX@users.noreply.github.com>
2026-06-13 06:49:32 +0000

    deploy: 10bd2cbb000dee64a779756b4539d76e7167a778

--- assets/Home-D3EvqzGr.js
+++ assets/Home-D3EvqzGr.js
@@ -1,4 +1,4 @@
-import{r as ue,j as v,u as un,L as hn}from"./index-B6wFyvga.js";/**
+import{r as ue,j as v,u as un,L as hn}from"./index-6v2Oy5rI.js";/**
  * Anime.js - core - ESM
  * @version v4.4.1
  * @license MIT
