65d54df954d58d78728fb5b3fd016ad6bfb3b135
therapon1997-DERMLUX <therapon1997-DERMLUX@users.noreply.github.com>
2026-07-07 18:22:04 +0000

    deploy: 73b1d669ea4051e7ecc0e09dba0b68001b2baf2f

--- assets/Home-71OIp_Uu.js
+++ assets/Home-71OIp_Uu.js
@@ -1,4 +1,4 @@
-import{r as ue,j as v,u as un,L as hn}from"./index-CjSuuTuG.js";/**
+import{r as ue,j as v,u as un,L as hn}from"./index-bm4KG48u.js";/**
  * Anime.js - core - ESM
  * @version v4.4.1
  * @license MIT
